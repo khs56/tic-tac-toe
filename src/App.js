5d9edd1314500298d917628e84b03593960c9f55
Vishal M <48333506+khs56@users.noreply.github.com>
2025-07-16 10:30:02 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 function Square({ value }) {
   return (
-    <button className="square">{value}</button>
+    <button className="square" onClick={onSquareClick}>{value}</button>
   )
 }
 
@@ -11,7 +11,7 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
+        <Square value={squares[0]} onSquareClick={handleClick} />
         <Square value={squares[1]} />
         <Square value={squares[2]} />
       </div>
