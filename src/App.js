2c95eb072a561ca14bfb3c97d796ae0dfb756658
Vishal M <48333506+khs56@users.noreply.github.com>
2025-07-16 14:34:06 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -57,6 +57,7 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true)
   const [history, setHistory] = useState([Array(9).fill(null)])
+  const [currentMove, setCurrentMove] = useState(0)
   const currentSquares = history[history.length - 1]
 
   function handlePlay(nextSquares) {
@@ -64,15 +65,32 @@ export default function Game() {
     setXIsNext(!xIsNext)
   }
 
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove)
+    setXIsNext(nextMove % 2 === 0)
+  }
+
+  const moves = history.map((squares, move) => {
+    let description
+    if (move > 0) {
+      description = 'Go to move #' + move
+    } else {
+      description = 'Go to game start'
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
+
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>
-          { /* to do */}
-        </ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   )
